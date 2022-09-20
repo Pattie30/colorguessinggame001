82f25ca47a2e8e95f963848a48dfa6977a84e8e0
Pattie30 <87343336+Pattie30@users.noreply.github.com>
2022-09-20 13:32:32 -0400

    daytwo

---
+++ script.js
@@ -0,0 +1,78 @@
+Colors_Array =  [
+    'blue = #00FFFF',
+    'green = 	#32CD32',
+    'lavander = #9873AC',
+    'orange =#FFA500 ',
+    'magenta = #FF00FF',
+    'mustard = #FFDB58'
+
+]
+
+let table = document.getElementById('table')
+let Btn1 = document.getElementById('Btn-One')
+let Btn2 = document.getElementById('Btn-Two')
+let Btn3 = document.getElementById('Btn-Three')
+let Btn4 = document.getElementById('Btn-Four')
+let Btn5 = document.getElementById('Btn-Five')
+let Btn6 = document.getElementById('Btn-Six')
+
+
+const rainbow = () => {
+    let A = (Math.floor(Math.random() * Colors_Array.length) )
+    let B = (Math.floor(Math.random() * Colors_Array.length) )
+    let C = (Math.floor(Math.random() * Colors_Array.length) )
+    let D = (Math.floor(Math.random() * Colors_Array.length) )
+    let E = (Math.floor(Math.random() * Colors_Array.length) )
+    let F = (Math.floor(Math.random() * Colors_Array.length) )
+    
+    
+   
+    table.appendChild[rainbow]
+   
+    
+    
+}
+rainbow()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
